9100f0def812ca28ccb91d1fe9b00bcf3d790a17
Brice LESNE <brice@mbp-de-brice.lan>
2020-08-20 11:19:11 +0200

    add some css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,20 @@ import React from "react";
 const Card = ({ data }) => {
   return (
     <div className="card-container">
-      <div className="card-avatar-username">
+      <div
+        onClick={() => alert("Go to user profile !")}
+        className="card-avatar-username"
+      >
         <img alt={data.title} src={data.user.picture} />
         <span>{data.user.username}</span>
       </div>
-      <img alt={data.title} src={data.picture} />
-      <div className="card-price-size-brand">
-        <span>{data.price} €</span>
-        <span>{data.size}</span>
-        <span>{data.brand}</span>
+      <div onClick={() => alert("Go to offer !")}>
+        <img alt={data.title} src={data.picture} />
+        <div className="card-price-size-brand">
+          <span>{data.price} €</span>
+          <span>{data.size}</span>
+          <span>{data.brand}</span>
+        </div>
       </div>
     </div>
   );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,27 +4,13 @@ import data from "../assets/data.json";
 
 import Card from "../components/Card";
 
-import hero from "../assets/hero.jpg";
 import forme from "../assets/forme.svg";
 
 const Home = () => {
   return (
     <>
-      <div
-        style={{
-          backgroundImage: `url(${hero})`,
-          height: 455,
-          width: "100%",
-          position: "relative",
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-        }}
-      >
-        <img
-          src={forme}
-          alt="forme"
-          style={{ position: "absolute", bottom: 0, right: 0, height: 80 }}
-        />
+      <div className="home-hero-bg-img">
+        <img src={forme} alt="forme" className="home-hero-forme" />
       </div>
 
       <div className="home-card-wrapper">
